bb2daf6e8d4c3033d0f05db6d1d1f7ba30fb79ae
ShrutiLendhe509 <shrutilendhe509@gmail.com>
2022-11-24 12:17:58 -0600

    Completed Problem1 & Problem2

---
+++ Problem 1.cs
@@ -0,0 +1,78 @@
+﻿
+using System.Collections.Generic;
+// Time Complexity :O(1)
+// Space Complexity :O(n)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :No
+public class MyHashSet
+{
+    int[] num;
+    public MyHashSet()
+    {
+        num = new int[100011];
+        for (int i = 0; i < num.Length; i++)
+        {
+            num[i] = -1; //intializing the default array value with -1
+        }
+    }
+
+    public void add(int key)
+    {
+        int index = GetHashIndex(key);
+
+        if (num[index] == key) //checking if key exists in  num[]
+        {
+            return; 
+        }
+        else if (num[index] == -1)
+        {
+            num[index] = key; //updating the key if not exist in num[]
+            return;
+        }
+
+    }
+
+    public void remove(int key)
+    {
+        int index = GetHashIndex(key);
+        if (num[index] == key)// if key exits
+            num[index] = -1;  //assign -1 for removing.
+        return;
+    }
+
+    public bool contains(int key)
+    {
+        int index = GetHashIndex(key);
+
+        if (num[index] == key) // key matches with num[i] value 
+            return true;    // return true         
+
+
+        return false;
+    }
+
+    public int GetHashIndex(int key)
+    {
+        return key % num.Length;
+    }
+
+    public static void Main(String[] args)
+    {
+        bool val = false;
+        MyHashSet hashSet = new MyHashSet();
+        hashSet.add(1);
+        hashSet.add(2);
+        val = hashSet.contains(1);    // returns true
+        Console.WriteLine(val);
+        val = hashSet.contains(3);    // returns false (not found)
+        Console.WriteLine(val);
+        hashSet.add(2);
+        val = hashSet.contains(2);    // returns true
+        Console.WriteLine(val);
+        hashSet.remove(2);
+        val = hashSet.contains(2);    // returns false (already removed)
+        Console.WriteLine(val);
+        Console.ReadLine();
+    }
+
+}
---
+++ Problem 2.cs
@@ -0,0 +1,62 @@
+﻿// Time Complexity :O(1)
+// Space Complexity :O(1)
+// Did this code successfully run on Leetcode :Yes
+// Any problem you faced while coding this :yes how to handle empty stack.
+public class MinStack
+{
+    private const int MaxValue = 2147483647;
+    private Stack<int> mainStack = new Stack<int>();
+    private Stack<int> minStack = new Stack<int>();
+    private int min = 2147483647;
+
+    public MinStack()
+    {
+        minStack.Push(min);
+    }
+
+    public void Push(int val)
+    {
+        mainStack.Push(val);
+        min = Math.Min(val, min);
+        minStack.Push(min);
+    }
+
+    public void Pop()
+    {
+        mainStack.Pop();
+        minStack.Pop();
+        min= minStack.Peek();
+
+    }
+
+    public int Top()
+    {
+        return mainStack.Peek();
+    }
+
+    public int GetMin()
+    {
+        return min;
+    }
+
+
+    public static void Main(string[] args)
+    {
+        MinStack obj = new MinStack();
+        obj.Push(5);
+        obj.Push(9);
+        obj.Push(4);
+        obj.Pop();
+        obj.Push(12);
+        obj.Pop();
+        obj.Push(3);
+        obj.Pop();
+        obj.Pop();
+        obj.Pop();
+        int param_3 = obj.Top();
+        int param_4 = obj.GetMin();
+        Console.WriteLine(param_3);
+        Console.WriteLine(param_4);
+        Console.ReadLine();
+    }
+}
